64dc645e5a3bb53d284485575af299e954fac850
Vaha_bc76@itstep.edu.az <Vaha_bc76@itstep.edu.az>
2021-10-20 19:15:49 +0400

    Add project files.

---
+++ 7_Borcdan_1cisi/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+namespace Abstract_Factory
+{
+    class Program
+    {
+        public interface ICheckbox
+        {
+            void Paint();
+        }
+        public interface IButton
+        {
+            void Paint();
+        }
+        public interface IGuiFactory
+        {
+            IButton CreateButton();
+            ICheckbox CreateCheckbox();
+        }
+
+
+        public class MacButton : IButton
+        {
+            public void Paint()
+            {
+                Console.WriteLine("MacBtn Created");
+            }
+        }
+        public class MacCheckbox : ICheckbox
+        {
+            public void Paint()
+            {
+                Console.WriteLine("MacCheckbox Created");
+
+            }
+        }
+        public class MacFactory : IGuiFactory
+        {
+            public IButton CreateButton() => new MacButton();
+            
+
+            public ICheckbox CreateCheckbox() => new MacCheckbox();
+        }
+
+        public class WinButton : IButton
+        {
+            public void Paint()
+            {
+                Console.WriteLine("WindowsBtn Created");
+            }
+        }
+        public class WinCheckbox : ICheckbox
+        {
+            public void Paint()
+            {
+                Console.WriteLine("WindowsCheckbox Created");
+
+            }
+        }
+        public class WinFactory : IGuiFactory
+        {
+            public IButton CreateButton() => new WinButton();
+            
+
+            public ICheckbox CreateCheckbox() => new WinCheckbox();
+        }
+
+
+        public class Applicaion
+        {
+            private IButton _button;
+            private ICheckbox _checkbox;
+            private readonly IGuiFactory _guiFactory;
+
+            public Applicaion(IGuiFactory guiFactory)
+            {
+                _guiFactory = guiFactory;
+            }
+
+            public void CreateUI()
+            {
+                _button = _guiFactory.CreateButton();
+                _checkbox = _guiFactory.CreateCheckbox();
+            }
+
+            public void Print()
+            {
+                _button.Paint();
+                _checkbox.Paint();
+            }
+
+                 
+        }
+      
+
+
+        public static void Main(string[] args)
+        {
+
+
+            
+            Applicaion winApp = new Applicaion(new WinFactory());
+            winApp.CreateUI();
+            winApp.Print();
+             
+            
+            Applicaion macApp = new Applicaion(new MacFactory());
+            macApp.CreateUI();
+            macApp.Print();
+
+         }
+    }
+}
